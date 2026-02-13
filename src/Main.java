3100c73e09e2723e660acfb3e739344374a779de
evdmatvey <evd.matvey@mail.ru>
2026-02-14 06:33:44 +0700

    Add role class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 void main() {
     try {
-        User u1 = User.validate("1", "Ivan Ivan", "ivanrj332@mail.ru");
+        User u1 = User.create("1", "Ivan Ivan", "ivanrj332@mail.ru");
 
         System.out.println(u1.format());
     } catch (Exception e) {
